d8305b50aa118caf39d52fab835dce0ee8245a77
Evgeniy <yaremenko.x@gmail.com>
2023-05-05 20:02:00 +0300

    add function getBookId

--- src/js/home/render-books-list.js
+++ src/js/home/render-books-list.js
@@ -4,6 +4,12 @@ const fetchApiCategories = new FetchCategoriesAll();
 
 fetchApiCategories.getCategoriesList().then(res => console.log(res));
 
+fetchApiCategories.getCategoriesTop().then(res => console.log(res));
+
 fetchApiCategories
   .getCategoriesSelected('Business Books')
+  .then(res => console.log(res));
+
+fetchApiCategories
+  .getBookId('643282b1e85766588626a087')
   .then(res => console.log(res));
