3dc3943361b24beadcc499f35c308d05872c7db6
Yuriy <yuriy.dobryi@gmail.com>
2023-05-06 18:04:24 +0300

    Revert "fix Images"

--- src/js/home/render-books-list.js
+++ src/js/home/render-books-list.js
@@ -2,14 +2,54 @@ import FetchCategoriesAll from './service-categories-all';
 
 const fetchApiCategories = new FetchCategoriesAll();
 
-fetchApiCategories.getCategoriesList().then(res => console.log(res));
+const booksList = document.querySelector('.books-list')
 
-fetchApiCategories.getCategoriesTop().then(res => console.log(res));
+const renderBooksList = async () => {
+  const categoriesTop = await fetchApiCategories.getCategoriesTop();
+  booksList.innerHTML = '';
 
-fetchApiCategories
-  .getCategoriesSelected('Business Books')
-  .then(res => console.log(res));
+  categoriesTop.forEach(category => {
+    const books = category.books.map(book => {
+      return `
+      <div class="book-card" data-book-id="${book._id}">
+          <img src="${book.book_image}" alt="${book.title}" class="book-image">
+          <div class="book-info">
+            <h3 class="book-title">${book.title}</h3>
+            <p class="book-autor">${book.author}</p>
+          </div>
+        </div>
+      `;
+    }).join('');
 
-fetchApiCategories
-  .getBookId('643282b1e85766588626a087')
-  .then(res => console.log(res));
+    const categorySection = `
+    <section class="category-section">
+      <h2>${category.list_name}</h2>
+      <div class="books-container">
+        ${books}
+      </div>
+      <button type=button>SEE MORE</button>
+    </section>`;
+
+    booksList.insertAdjacentHTML('beforeend', categorySection);
+  });
+  booksList.addEventListener('click', event => {
+    const bookCard = event.target.closest('.book-card');
+    const bookId = bookCard.dataset.bookId;
+    console.log(bookId);
+  });
+};
+
+renderBooksList();
+
+// fetchApiCategories.getCategoriesList().then(res => console.log(res));
+
+// fetchApiCategories.getCategoriesTop().then(res => console.log(res));
+
+// fetchApiCategories
+//   .getCategoriesSelected('Business Books')
+//   .then(res => console.log(res));
+
+// fetchApiCategories
+//   .getBookId('643282b1e85766588626a087')
+//   .then(res => console.log(res));
+
