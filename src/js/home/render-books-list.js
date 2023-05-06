856bde96858861c0fa8b8c090ffa22ba577d35de
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-05-07 01:15:37 +0300

    Update render-books-list.js

--- src/js/home/render-books-list.js
+++ src/js/home/render-books-list.js
@@ -1,4 +1,5 @@
 import FetchCategoriesAll from './service-categories-all';
+// import { requestCard } from './modal-card';
 
 const fetchApiCategories = new FetchCategoriesAll();
 
@@ -27,19 +28,63 @@ const renderBooksList = async () => {
       <div class="books-container">
         ${books}
       </div>
-      <button type=button>SEE MORE</button>
+      <button type=button data-category-books="${category.list_name}" class="category-books-button">SEE MORE</button>
     </section>`;
 
     booksList.insertAdjacentHTML('beforeend', categorySection);
-  });
+  });  
+  
   booksList.addEventListener('click', event => {
     const bookCard = event.target.closest('.book-card');
-    const bookId = bookCard.dataset.bookId;
-    console.log(bookId);
+    if (bookCard) {
+      const bookId = bookCard.dataset.bookId;
+      // requestCard(bookId);
+      console.log(bookId);
+    }
+  });
+  
+  booksList.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('category-books-button')) {
+      const category = event.target.dataset.categoryBooks;
+      console.log(category);
+
+      renderBooksListCategori(category);
+    }
   });
 };
 
 renderBooksList();
+
+const renderBooksListCategori = async (category) => {
+  const booksListCategori = await fetchApiCategories.getCategoriesSelected(category);
+  booksList.innerHTML = '';
+
+  const books = booksListCategori.map(book => {
+    return `
+    <div class="book-card" data-book-id="${book._id}">
+        <img src="${book.book_image}" alt="${book.title}" class="book-image">
+        <div class="book-info">
+          <h3 class="book-title">${book.title}</h3>
+          <p class="book-autor">${book.author}</p>
+        </div>
+    </div>
+    `;
+  }).join('');
+
+  const booksSection = `
+    <section class="category-section">
+      <h1>${category}</h1>
+      <div class="books-container">
+        ${books}
+      </div>
+    </section>
+  `;
+
+  booksList.insertAdjacentHTML('beforeend', booksSection);
+
+  window.scrollTo(0, 0);
+};
+
 
 // fetchApiCategories.getCategoriesList().then(res => console.log(res));
 
