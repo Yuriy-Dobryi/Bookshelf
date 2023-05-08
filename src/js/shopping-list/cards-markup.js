7845bf3bd862b1cdee7bfb34c4eecc4789eaa15f
Yuriy-Dobryi <yuriy.dobryi@gmail.com>
2023-05-09 01:21:15 +0300

    localStorage Logic in Shopping List
    render books from storage or empty page

--- src/js/shopping-list/cards-markup.js
+++ src/js/shopping-list/cards-markup.js
@@ -1,47 +1,31 @@
-const shoppingListEl = document.querySelector('.shopping-list');
+import books from '../../images/book-logo/bookLogo-tablet@2x.png';
 
-function createImgCardsMarkup() {
-  shoppingListEl.innerHTML = shoppingList
-    .map(() => {
-      return `<li class="gallery__item">
-          <a class="gallery__link" href="large-image.jpg">
-              <img
-              class="gallery__image"
-              src=""
-              alt=""
-              />
-          </a>
-          <div class="div_card">
-          <h2 class="title_card">I will find you</h2>
-          <p class="card_min-title">Hardcover fiction</p>
-          <p class="card_text">David Burroughs was once a devoted father to his three-year-old son Matthew, living a dream life just a short drive away from the working-class suburb where he and his wife, Cheryl, first fell in love--until one fateful night when David woke suddenly to discover Matthew had been murdered while David was asleep just down the hall.</p>
-          <p class="card_avter">Harlan Coben</p>
-          </div>
-          <div class="div_svg-card">
-          <a href="#">
-            <svg class="" width="48" height="15">
-                <use href="./img/sprite.svg#icon-amazon"></use>
-            </svg>
-            </a>
-            <a href="#">
-            <svg class="" width="48" height="15">
-                <use href="./img/sprite.svg#icon-book"></use>
-            </svg>
-            </a>
-            <a href="#">
-            <svg class="" width="48" height="15">
-                <use href="./img/sprite.svg#icon-bookshop"></use>
-            </svg>
-            </a>
-            <div class="div-card_close">
-            <a href="#">
-            <svg class="" width="34" height="34">
-                <use href="#"></use>
-            </svg>
-            </a>
-            </div>
+const bookList = JSON.parse(localStorage.getItem('SHOPPING-BOOKS-LIST'));
+const shoppingListRef = document.querySelector('.shopping-list-books');
 
-      </li>`;
-    })
-    .join('');
+export function renderShoppingList() {
+  if (bookList) {
+    // renderShoppingCardList(bookList);
+  } else {
+    renderEmptyShoppingList();
+  }
 }
+
+function renderShoppingCardList(bookList) {
+  shoppingListRef.innerHTML = bookList
+    .map(
+      ({ book }) => `
+      заміни цей текст на свою розмітку
+      `
+    )
+    .join();
+}
+
+function renderEmptyShoppingList() {
+  shoppingListRef.innerHTML = `
+  <p>This page is empty, add some books and proceed to order.</p>
+  <img src=${books}>
+  `;
+}
+
+renderShoppingList();
