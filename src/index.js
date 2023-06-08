164a3fa0d959b7f4de73c44327bae13e6476ad01
Yuriy-Dobryi <yuriy.dobryi@gmail.com>
2023-06-08 13:41:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './js/menu-header/menu-header.js';
 import './js/support/support.js';
 import './js/home/render-categories-list.js';
 import './js/home/render-books-list.js';
-import './js/home/modal-card';
 import './js/home/scroll-up.js';
+import './js/home/modal-card';
